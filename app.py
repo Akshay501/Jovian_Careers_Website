23c9ccd1ef463376d5fbe19960c2342141831210
Akshay Kumar <31762924+Akshay501@users.noreply.github.com>
2025-11-04 23:15:19 +0000

    Added my first flask project

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '''
-    <html>
-        <head>
-            <title>Jovian Careers</title>
-        </head>
-        <body>
-            <h1>Welcome to Jovian Careers</h1>
-            <p>This is a career website for Jovian.</p>
-        </body>
-    </html>
-    '''
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route("/")
+def hello_world():
+  return """<p>
+  Hello, World!<br>
+    
+  This is Akshay  
+  </p>"""
+
+
+print(__name__)
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
